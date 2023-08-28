55ef7d8fdb1c28f03e9261729952c4f38d9c83ac
JordanLerner <jordanlerner08@gmail.com>
2023-08-27 21:19:19 -0700

    Sprites added

--- example.py
+++ example.py
@@ -1,8 +1,12 @@
+import keyboard
+
 import pgt
 
-
 game = pgt.PGT(700, 450)
-game.set_background_color("white")
+game.set_background_color("gray")
+game.set_up_key("w")
+
+game.create_sprite("test_sprite", 5, 5, 4, True, game)
 
 running = True
 
@@ -10,6 +14,10 @@ running = True
 while running:
     pg = game.pygame
     game.repaint()
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             quit()
+
+    game.set_current_key(keyboard.read_key())
+    print(keyboard.read_key())
--- pgt.py
+++ pgt.py
@@ -1,5 +1,7 @@
 import datetime
+import os
 
+import pygame
 import pygame as pg
 
 pg.init()
@@ -15,17 +17,66 @@ class PGT:
         self.pygame = pg
         self.screen = pg.display.set_mode((self.winX, self.winY))
 
+        self.current_key = ""
+        self.movement_keys = {
+            "up": (0, 1),
+            "down": (0, -1),
+            "left": (-1, 0),
+            "right": (1, 0)
+        }
         self.sprites = []
+        self.bg_color = "white"
 
     def set_background_color(self, color):
-        self.screen.fill(color)
+        self.bg_color = color
+
+    def create_sprite(self, image_name, x_pos, y_pos, speed, is_controlled, game_class):
+        new_sprite = Sprite()
+        new_sprite.image = image_name
+        new_sprite.xPos = x_pos
+        new_sprite.yPos = y_pos
+        new_sprite.speed = speed
+        new_sprite.isControlled = is_controlled
+        new_sprite.gameClass = game_class
+        self.sprites.append(new_sprite)
+
+    def set_current_key(self, key):
+        self.current_key = key
+
+    def set_up_key(self, key):
+        self.movement_keys[0] = key
+
+    def set_down_key(self, key):
+        self.movement_keys[1] = key
+
+    def set_left_key(self, key):
+        self.movement_keys[2] = key
+
+    def set_right_key(self, key):
+        self.movement_keys[3] = key
 
     def repaint(self):
+        self.screen.fill(self.bg_color)
+
         for sprite in self.sprites:
-            self.screen.blit(sprite.image, (sprite.xPos, sprite.yPos))
+            if sprite.isControlled:
+                sprite.check_move(self.current_key)
+
+            self.screen.blit(pygame.image.load(os.path.join("textures\\" + sprite.image + ".png")),
+                             (sprite.xPos, sprite.yPos))
+
+        pg.display.flip()
 
 
 class Sprite:
+    image: ""
     xPos: 0
     yPos: 0
-    image: ""
+    speed: 1
+    isControlled: True
+    gameClass: PGT
+
+    def check_move(self, key):
+        if key in self.gameClass.movement_keys:
+            self.xPos += self.gameClass.movement_keys[key][0] * self.speed
+            self.yPos += self.gameClass.movement_keys[key][1] * -self.speed
